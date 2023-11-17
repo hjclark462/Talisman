e57d982930febd13b0688c6f4994290c2929feb7
SBgamedesign <sbenson@live.com.au>
2023-11-17 11:11:28 +1100

    Updated Giant Statue Material with grey statue texture. Updated Subtitles colours. Updated subtitle background colour in menumanager.

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -655,7 +655,7 @@ public class MenuManager : MonoBehaviour
         {
             m_subtitles.color = m_swordColour;
         }
-        m_subtitles.text = "<mark=#000000B4 padding=\"2,2,15,12\">" + subtitile + "</mark>";        
+        m_subtitles.text = "<mark=#000000DC padding=\"2,2,15,12\">" + subtitile + "</mark>";        
     }    
 
     public async UniTask HitReticle()
